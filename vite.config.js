6ea3ec384594002af5903d4f5289fc346a55581d
Eugeniya <paladina.em1@kvt.su>
2025-12-02 11:51:17 +0300

    Добавлены настройки для сборки

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
+import createSvgSpritePlugin from 'vite-plugin-svg-spriter'
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+const FRONT_PATH = 'src';
 
 export default defineConfig({
     root: 'src',
     base: "/",
+    build: {
+        minify: true,
+        cssMinify: true,
+        rolldownOptions: {
+            input: {
+                index: resolve(__dirname, `${FRONT_PATH}/index.html`),
+            }
+        }
+    }
 });
