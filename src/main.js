07a564e2e24c25017750d22fc017a99c4193a339
Eugeniya <paladina.em1@kvt.su>
2025-12-02 12:07:22 +0300

    Добавлены настройки

--- src/main.js
+++ src/main.js
@@ -1,39 +1,8 @@
-import './style.css'
+// import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '../public/vite.svg'
 import { setupCounter } from './counter.js'
 
-import { waapi, splitText, createTimer,  animate, svg, stagger} from 'animejs';
-
-const { chars } = splitText('h2', { words: false, chars: true });
-
-waapi.animate(chars, {
-  translate: `0 -2rem`,
-  delay: stagger(100),
-  duration: 600,
-  loop: true,
-  alternate: true,
-  ease: 'inOut(2)',
-});
-   
-// const [ $time, $count ] = utils.$('.value');
-
-// createTimer({
-//   duration: 1000,
-//   loop: true,
-//   frameRate: 30,
-//   onUpdate: self => $time.innerHTML = self.currentTime,
-//   onLoop: self => $count.innerHTML = self._currentIteration
-// });
-
-
-animate(svg.createDrawable('.line'), {
-  draw: ['0 0', '0 1', '1 1'],
-  ease: 'inOutQuad',
-  duration: 2000,
-  delay: stagger(100),
-  loop: true
-});
 
 document.querySelector('#app').innerHTML = `
   <div>
